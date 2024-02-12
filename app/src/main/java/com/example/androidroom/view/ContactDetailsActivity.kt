af05d2ee7a71432adae9fc04d8858620aef0f0f5
Luiz Furmann <furmannluiz@gmail.com>
2024-02-12 14:43:37 -0300

    mostrando lista de contatos na tela inicial

---
+++ app/src/main/java/com/example/androidroom/view/ContactDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidroom.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidroom.R
+
+class ContactDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_details)
+    }
+}
--- app/src/main/java/com/example/androidroom/view/MainActivity.kt
+++ app/src/main/java/com/example/androidroom/view/MainActivity.kt
@@ -2,14 +2,49 @@ package com.example.androidroom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.androidroom.databinding.ActivityMainBinding
+import com.example.androidroom.model.Contact
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private val contactViewModel: ContactViewModel by viewModels()
+    private val contactAdapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setupViewModel()
+        setupRecyclerView()
+    }
+
+    private fun setupViewModel() {
+        contactViewModel.readAllData().observe(this) { contacts ->
+            updateList(contacts)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvContact.layoutManager = layoutManager
+        binding.rvContact.adapter = contactAdapter
+        binding.rvContact.setHasFixedSize(true)
+    }
+
+    private fun updateList(contacts: List<Contact>) {
+        if (contacts.isEmpty()) {
+            binding.rvContact.visibility = View.GONE
+        } else {
+            binding.rvContact.visibility = View.VISIBLE
+            contactAdapter.updateList(contacts)
+        }
     }
 }
