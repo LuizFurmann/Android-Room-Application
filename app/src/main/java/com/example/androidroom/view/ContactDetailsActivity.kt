385cc9c96d5abb1abbe6c8e7166bd76961bbfc61
Luiz Furmann <furmannluiz@gmail.com>
2024-02-12 14:53:26 -0300

    criado layout para criar e editar contato

--- app/src/main/java/com/example/androidroom/view/ContactDetailsActivity.kt
+++ app/src/main/java/com/example/androidroom/view/ContactDetailsActivity.kt
@@ -3,10 +3,15 @@ package com.example.androidroom.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.androidroom.R
+import com.example.androidroom.databinding.ActivityContactDetailsBinding
 
 class ContactDetailsActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityContactDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityContactDetailsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_details)
+        setContentView(binding.root)
     }
 }
